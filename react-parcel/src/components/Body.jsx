18c4acdfa097463d0f2f6595fb29cdc9bd829446
itsAhesan <ahesanchowdhury2016@gmail.com>
2025-12-29 10:27:07 +0530

    feat: add React state and filter top-rated restaurants

--- react-parcel/src/components/Body.jsx
+++ react-parcel/src/components/Body.jsx
@@ -1,15 +1,33 @@
 import Card from "./RestoCard";
 import resobj from "../utility/constants";
+import { useState } from "react";
 
 let Body = () => {
+
+
+  const [listOfResturants, setListOfResturants]= useState(resobj);
+
   return (
     <div className="body">
      <h1>Pizza</h1>
       <h3>Cheesilicious pizzas to make every day extraordinary.</h3>
       <h5>Restaurants to explore</h5>
-
+      <div>
+      <button 
+      onClick={()=>{
+        let filterData=listOfResturants.filter((restaurant)=>
+        restaurant.card.card.info.avgRating >= 4.5  
+        )
+        setListOfResturants(filterData);
+        console.log(filterData);
+        
+        
+      }}
+      
+      >Top Rated</button>
+      </div>
       <div className="food-items">
-        {resobj.map((restaurant) => (
+        {listOfResturants.map((restaurant) => (
           <Card
             key={restaurant.card.card.info.id}
             resobj={restaurant}
